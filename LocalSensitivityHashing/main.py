4189e3bde4e22f5c4372d0d742689637245ff7af
stefan duprey <stefan@swissborg.com>
2024-05-15 10:50:16 +0200

    go

---
+++ LocalSensitivityHashing/main.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import os
+import streamlit as st
+import pickle
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from dotenv import load_dotenv
+load_dotenv('.')  # take environment variables from .env (especially openai api key)
+api_key='sk-proj-MCwckUCnAiQPlYhXx2XbT3BlbkFJtqdsqAjxewbfeA4QQaik'
+os.environ["OPENAI_API_KEY"] = api_key
+st.title("SwissBorg: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+risk_report_df = pd.read_excel('./riskReports.xlsx')
+urls = list(risk_report_df['blog url'])
+urls=urls[:5]
+# counter=1
+# for me_link in urls:
+#     url = st.sidebar.text_input(f"RR {counter+1}",value=me_link)
+#     urls.append(url)
+#     counter=counter+1
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_store_openai.pkl"
+
+main_placeholder = st.empty()
+llm = OpenAI(temperature=0.9, max_tokens=500)
+process_url_clicked = True
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    embeddings = OpenAIEmbeddings()
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a pickle file
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_openai, f)
+    raise Exception('you goddam motherfucker')
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
+
+
+
+
